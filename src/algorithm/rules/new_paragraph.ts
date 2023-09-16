c620586b248e3b64a023f22b27ce88e014e23407
pseusys <shveitsar215@gmail.com>
2023-09-16 23:20:52 +0200

    grouping fixed

--- src/algorithm/rules/new_paragraph.ts
+++ src/algorithm/rules/new_paragraph.ts
@@ -2,13 +2,12 @@ import { Rule } from '../primitives';
 
 export class ParagraphRule extends Rule {
     private paragraphStart = '\t\u2014 ';
-    private paragraphRegexp = /(?:^|\n)\s*[\u002D|\u2010|\u2013]\s?/gmu;
+    private paragraphRegexp = /(^|\n)\s*[\u002D|\u2010|\u2013]\s?/gmu;
 
     public override apply(value: string): string {
         let match;
         while (match = this.paragraphRegexp.exec(value)) {
-            const nv = value.slice(0, match.index + (match.index == 0 ? 0 : 1));
-            value = nv + this.paragraphStart + value.slice(match.index + match[0].length);
+            value = value.slice(0, match.index + match[1].length) + this.paragraphStart + value.slice(match.index + match[0].length);
         }
         return value;
     }
--- src/algorithm/rules/quote_replace.ts
+++ src/algorithm/rules/quote_replace.ts
@@ -6,16 +6,16 @@ export class QuoteReplaceRule extends Rule {
     private innerOpenQuote = '\u201C';
     private innerCloseQuote = '\u201D';
 
-    private openQuoteRegexp = /(?:^|\n|\s)[\u201C|\u00AB|\u201E|\u0022|\u0027]/gmu;
-    private closeQuoteRegexp = /\S[\u201D|\u00BB|\u201F|\u0022|\u0027](?:$|\n|\s|\p{P})/gmu;
+    private openQuoteRegexp = /(^|\n|\s)[\u201C|\u00AB|\u201E|\u0022|\u0027]/gmu;
+    private closeQuoteRegexp = /\S[\u201D|\u00BB|\u201F|\u0022|\u0027]($|\n|\s|\p{P})/gmu;
     private innerQuoteRegexp = /(\u00AB[\S|\s]+)(\u00AB[\s|\S]+\u00BB)([\S|\s]+\u00BB)/gmu;
 
     public override apply(value: string): string {
         let match;
         while (match = this.openQuoteRegexp.exec(value))
-            value = value.slice(0, match.index + (match.index == 0 ? 0 : 1)) + this.outerOpenQuote + value.slice(match.index + match[0].length);
+            value = value.slice(0, match.index + match[1].length) + this.outerOpenQuote + value.slice(match.index + match[0].length);
         while (match = this.closeQuoteRegexp.exec(value))
-            value = value.slice(0, match.index + 1) + this.outerCloseQuote + value.slice(match.index + match[0].length - (match.index + match[0].length == value.length ? 0 : 1));
+            value = value.slice(0, match.index + 1) + this.outerCloseQuote + value.slice(match.index + match[0].length - match[1].length);
         while (match = this.innerQuoteRegexp.exec(value)) {
             const innserStart = match.index + match[1].length
             const innserEnd = match.index + match[1].length + match[2].length 
